1944c2e897bb3bd2c1dfbae8a75fb6b2f1c597f4
NiceLionel <90742138+NiceLionel@users.noreply.github.com>
2023-09-21 18:30:29 -0400

    Update est_pixel_world.py

--- code/est_pixel_world.py
+++ code/est_pixel_world.py
@@ -13,7 +13,16 @@ def est_pixel_world(pixels, R_wc, t_wc, K):
         Pw: N x 3 points, the world coordinates of pixels
     """
 
-    ##### STUDENT CODE START #####
-
-    ##### STUDENT CODE END #####
+    
+    t = -R_wc.T.dot(t_wc)
+    R = R_wc.T
+    intermid = np.matmul(K, np.hstack((R[:,0].reshape(3,1), R[:,1].reshape(3,1), t.reshape(3,1))))
+    pc = np.hstack((pixels, np.ones(pixels.shape[0]).reshape(-1,1)))
+    P = np.linalg.inv(intermid) @ pc.T
+    Pw = np.zeros((pixels.shape[0],3))
+    for i in range(len(pixels)):
+        pc_ = P[:,i]
+        pw_1 = pc_[1]/pc_[2]
+        pw_2 = pc_[0]/pc_[2]
+        Pw[i] = np.array([pw_2, pw_1,0])
     return Pw
