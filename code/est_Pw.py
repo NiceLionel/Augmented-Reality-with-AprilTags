e7cf437af8c25056d4a038fafb6d155f1fe242c9
NiceLionel <90742138+NiceLionel@users.noreply.github.com>
2023-09-21 18:30:58 -0400

    Update est_Pw.py

--- code/est_Pw.py
+++ code/est_Pw.py
@@ -15,7 +15,7 @@ def est_Pw(s):
     """
 
     ##### STUDENT CODE START #####
-
+    Pw = np.array([[-s/2,-s/2,0],[s/2,-s/2,0],[s/2,s/2,0],[-s/2,s/2,0]])
     ##### STUDENT CODE END #####
 
     return Pw
